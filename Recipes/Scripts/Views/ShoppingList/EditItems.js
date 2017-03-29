e3d49ea0c74a18ff8fd176647cb38037f20e4aad
emorrison1962 <eric.morrison@gmail.com>
2017-03-28 23:28:14 -0500

    032817 11pm

--- Recipes/Scripts/Views/ShoppingList/EditItems.js
+++ Recipes/Scripts/Views/ShoppingList/EditItems.js
@@ -2,12 +2,32 @@
 
 var shoppingListEditItemsController = myApp.controller("shoppingListEditItemsController", ['$scope', '$window', '$log', '$http', '$location', function ($scope, $window, $log, $http, $location) {
 
+    var vm = this;
     $scope.isBusy = true;
+
+    $scope.styles = [];
+    $scope.styles["<Unknown>"] = "{ 'background-color': 'rgba(0,0,0,0.1)' }";
+    $scope.styles["Produce"] = "{ 'background-color': 'rgba(0,255,0,0.3)' }";
+    $scope.styles["Meat"] = "{ 'background-color': 'rgba(255,0,0,0.3)' }";
+    $scope.styles["Dairy"] = "{ 'background-color': 'rgba(255,255,0,0.50)' }";
+    $scope.styles["Deli"] = "{ 'background-color': 'rgba(252,158,0,0.50)' }";
+    $scope.styles["Soap"] = "{ 'background-color': 'rgba(25,182,255,0.39)' }";
+    $scope.styles["Paper"] = "{ 'background-color': 'rgba(255,0,0,0.1)' }";
+    $scope.styles["Sam's"] = "{ 'background': 'linear-gradient(to right, #3b6799 0%,#fcfcfc 20%,#fff2f2 80%,#ff5e5e 100%);' }";
+    $scope.styles["Other"] = "{ 'background-color': 'rgba(255,0,0,0.1)' }";
+
 
     $scope.init = function (model) {
         $scope.model = model;
+        $scope.selectedItems = [];
         $log.debug($scope.model);
         $scope.setGroupStyles();
+
+        $scope.model.forEach(function (group) {
+            group.Items.forEach(function(item) {
+                item.Group = group;
+            });
+        });
 
 
     };
@@ -23,9 +43,58 @@ var shoppingListEditItemsController = myApp.controller("shoppingListEditItemsCon
     $scope.setGroupStyles = function ()
     {
         $scope.model.forEach(function (group) {
-            //if (group.Text == )
-            group.Style = "{ 'background-color': 'blue' }";
+            var style = $scope.styles[group.Text];
+            group.Style = style;
+            group.Items.forEach(function (item) {
+                item.Style = style;
+            });
         });
+    };
+
+    $scope.groupChecked = function (group) {
+        vm.selectedGroup = group;
+        //$scope.setGroup();
+    };
+
+    $scope.setGroup = function () {
+        if (undefined !== $scope.selectedGroup) {
+            if ($scope.selectedItems.length > 0) {
+                $scope.selectedItems.forEach(function (item) {
+                    item.Group.Items.RemoveGroupItem(item);
+                    $scope.selectedGroup.Items.push(item);
+                    item.Style = $scope.selectedGroup.Style;
+                    item.Group = $scope.selectedGroup;
+                });
+            }
+        }
+    };
+
+    Array.prototype.RemoveGroupItem = function (item) {
+        for (i = 0; i < this.length; i++) {
+            if (this[i].ShoppingListItemId === item.ShoppingListItemId) {
+                this.splice(i, 1);
+                break;
+            }
+        }
+    };
+
+
+
+    $scope.itemChecked = function (item) {
+        if (item.IsSelected)
+            $scope.selectedItems.push(item);
+        else
+            $scope.selectedItems.RemoveSelectedItem(item);
+        $scope.setGroup();
+    };
+
+    Array.prototype.RemoveSelectedItem = function (item) {
+        for (i = 0; i < this.length; i++) {
+            if (this[i].ShoppingListItemId === item.ShoppingListItemId) {
+                this.splice(i, 1);
+                break;
+            }
+        }
     };
 
     //////////////////////////////////////////////////////////////////////////////////////
