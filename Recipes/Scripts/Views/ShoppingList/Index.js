7984a7eb2b024608c9d28d56b5d7f0729ed860c1
emorrison1962 <eric.morrison@gmail.com>
2017-05-27 17:52:46 -0500

    052717 02 Title case shopping list items.

--- Recipes/Scripts/Views/ShoppingList/Index.js
+++ Recipes/Scripts/Views/ShoppingList/Index.js
@@ -60,6 +60,17 @@ var shoppingListIndexController = myApp.controller("shoppingListIndexController"
         vm.newItem = "";
     }
 
+    $scope.newItem_Changed = function titleCase() {
+        var splitStr = vm.newItem.toLowerCase().split(' ');
+        for (var i = 0; i < splitStr.length; i++) {
+            // You do not need to check if i is larger than splitStr length, as your for does that for you
+            // Assign it back to the array
+            splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+        }
+        // Directly return the joined string
+        vm.newItem = splitStr.join(' ');
+    }
+
     $scope.saveShoppingList = function () {
         $http({
             method: 'POST',
